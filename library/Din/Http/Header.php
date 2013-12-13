0a6b7292686068358fca108b19bbe2dccec7e15e
Mário Mello <Mrio Mello@192.168.25.2>
2013-12-13 16:01:56 -0200

    Comentários na classe Header

--- library/Din/Http/Header.php
+++ library/Din/Http/Header.php
@@ -2,9 +2,17 @@
 
 namespace Din\Http;
 
+/**
+ * Classe Header
+ * Trabalha com o Header do protocolo HTTP
+ */
 class Header
 {
 
+  /**
+   * Redireciona browser
+   * @param String $url - url de destino do browser
+   */
   public static function redirect ( $url )
   {
     header("Location: {$url}");
