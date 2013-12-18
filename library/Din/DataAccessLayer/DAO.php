b07879928a573b76385ce4ade0f29d95f8c55f52
Jair Cueva Junior <junior@dindigital.com>
2013-12-18 17:19:57 -0200

    refatoração da class DAO

--- library/Din/DataAccessLayer/DAO.php
+++ library/Din/DataAccessLayer/DAO.php
@@ -4,27 +4,39 @@ namespace Din\DataAccessLayer;
 
 use Din\DataAccessLayer\PDO\PDODriver;
 use Din\DataAccessLayer\Table\iTable;
+use Din\DataAccessLayer\Criteria;
+use Din\DataAccessLayer\Select;
+use \Exception;
 
-/**
- * Class DAO
- *
- * @package default
- * @author
- */
 class DAO
 {
 
+  /**
+   * Instancia PDODriver
+   * @var object
+   */
   public $_driver;
 
+  /**
+   * Data Access Object para operações CRUD.
+   * @param \Din\DataAccessLayer\PDO\PDODriver $PDO
+   */
   public function __construct ( PDODriver $PDO )
   {
     $this->_driver = $PDO;
   }
 
-  public function insert ( iTable $Table, $ignore = false )
+  /**
+   * Realiza insert recebendo instancia da table como parâmetro.
+   * Retorna o lastInsertId
+   * @param \Din\DataAccessLayer\Table\iTable $table
+   * @param bool $ignore ignorar ao inserir id duplicado?
+   * @return int
+   */
+  public function insert ( iTable $table, $ignore = false )
   {
-    $tbl = $Table->getName();
-    $row = $Table->getArray();
+    $tbl = $table->getName();
+    $row = $table->getArray();
 
     $stmt = implode(', ', array_fill(0, count($row), '?'));
     $fields = implode(', ', array_keys($row));
@@ -41,10 +53,17 @@ class DAO
     return $last_insert_id;
   }
 
-  public function update ( iTable $Table, $arrCriteria )
+  /**
+   * Realiza update recebendo  instancia de table como parametro.
+   * Retorna o número de linhas afetadas.
+   * @param \Din\DataAccessLayer\Table\iTable $Table
+   * @param array $arrCriteria criterio no formato da class Criteria
+   * @return int
+   */
+  public function update ( iTable $table, array $arrCriteria )
   {
-    $tbl = $Table->getName();
-    $row = $Table->getArray();
+    $tbl = $table->getName();
+    $row = $table->getArray();
 
     $arr_stmt = array();
     foreach ( array_keys($row) as $k ) {
@@ -53,32 +72,45 @@ class DAO
 
     $stmt = implode(', ', $arr_stmt);
 
-    $SQL = "UPDATE {$tbl} SET {$stmt} {\$strWhere} ";
+    $criteria = new Criteria($arrCriteria);
 
-    $CriteriaMaker = new CriteriaMaker($arrCriteria, $SQL, $Table);
-    $arrIn = $CriteriaMaker->getArrIn();
-    $SQL = $CriteriaMaker->getSQL();
+    $SQL = "UPDATE {$tbl} SET {$stmt} " . $criteria->getSQL();
 
-    $arrParams = array_merge(array_values($row), $arrIn);
+    $arrParams = array_merge(array_values($row), $criteria->getArrIn());
+
+    $this->safe_operation($criteria->getSQL());
 
     $PDOStatement = $this->_driver->execute($SQL, $arrParams);
 
     return $PDOStatement->rowCount();
   }
 
-  public function delete ( $tablename, $arrCriteria )
+  /**
+   * Realiza delete recebendo nome da tabela e criteria.
+   * Retorna o número de linhas afetadas.
+   * @param string $tablename
+   * @param array $arrCriteria criterio no formato da class Criteria
+   * @return int
+   * @throws Exception
+   */
+  public function delete ( $tablename, array $arrCriteria )
   {
-    $SQL = "DELETE FROM {$tablename} {\$strWhere}";
+    $criteria = new Criteria($arrCriteria);
 
-    $CriteriaMaker = new CriteriaMaker($arrCriteria, $SQL);
-    $arrIn = $CriteriaMaker->getArrIn();
-    $SQL = $CriteriaMaker->getSQL();
+    $SQL = "DELETE FROM {$tablename} " . $criteria->getSQL();
 
-    $PDOStatement = $this->_driver->execute($SQL, $arrIn);
+    $this->safe_operation($SQL);
+    $PDOStatement = $this->_driver->execute($SQL, $criteria->getArrIn());
 
     return $PDOStatement->rowCount();
   }
 
+  /**
+   * Realiza select utilizando instancia da class Select como parametro
+   * Retorna resultado em array
+   * @param \Din\DataAccessLayer\Select $select
+   * @return array
+   */
   public function select ( Select $select )
   {
     $arrIN = $select->getWhereValues();
@@ -86,6 +118,12 @@ class DAO
     return $this->_driver->select($select->getSQL(), $arrIN);
   }
 
+  /**
+   * Realiza SELECT COUNT utilizando instancia da class Select como parametro
+   * Retorna um número inteiro.
+   * @param \Din\DataAccessLayer\Select $select
+   * @return int
+   */
   public function select_count ( Select $select )
   {
     $arrIN = $select->getWhereValues();
@@ -95,17 +133,32 @@ class DAO
     return intval($result[0]['total']);
   }
 
-  public function execute ( $SQL, $arrCriteria = array() )
+  /**
+   * Executa uma query livre, passando por parametro ela e o criterio.
+   * Retorna o número de linhas afetadas.
+   * @param string $SQL
+   * @param array $arrCriteria criterio no formato da class Criteria
+   * @return int
+   */
+  public function execute ( $SQL, array $arrCriteria = array() )
   {
-    $CriteriaMaker = new CriteriaMaker($arrCriteria, $SQL);
-    $arrIn = $CriteriaMaker->getArrIn();
-    $SQL = $CriteriaMaker->getSQL();
+    $criteria = new Criteria($arrCriteria);
+    $SQL .= ' ' . $criteria->getSQL();
 
-    $PDOStatement = $this->_driver->execute($SQL, $arrIn);
+    $PDOStatement = $this->_driver->execute($SQL, $criteria->getArrIn());
 
     return $PDOStatement->rowCount();
   }
 
-}
+  /**
+   * Bloqueia execução em caso de execução de query com where nulo
+   * @param string $SQL
+   * @throws Exception
+   */
+  private function safe_operation ( $SQL )
+  {
+    if ( !strpos($SQL, '=') && !strpos($SQL, '<') && !strpos($SQL, '>') )
+      throw new Exception('Necessário operador de comparação na query: ' . $SQL);
+  }
 
-// END
+}
