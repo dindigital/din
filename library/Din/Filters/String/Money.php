bd1db94d1ed4d535725bc94e92fb4dba24d79f6d
Mario Mello <mario@dindigital.com>
2013-12-20 16:33:06 -0200

    Separando StringTransform em filters string e removendo validation

---
+++ library/Din/Filters/String/Money.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Din\Filters\String;
+
+class Money
+{
+
+  /**
+   * Pega somente os números e retorna em formato float
+   * Exemplo: 'R$ 15.000,99' retorna 15000.99
+   *
+   * @param string $str
+   * @return float
+   */
+  public static function filter_sql ( $str )
+  {
+    return preg_replace("/[^0-9]/", '', $str) / 100;
+  }
+
+  /**
+   * Pega um número float e transforma em moeda BR
+   * Exemplo: '1000.00' retorna 'R$ 1.000,00'
+   *
+   * @param string $str
+   * @return string
+   */
+  public static function filter ( $str, $prefix = true )
+  {
+    $r = doubleval($str);
+    $r = number_format($r, 2, ',', '.');
+    if ( $prefix )
+      $r = 'R$ ' . $r;
+
+    return $r;
+  }
+
+}
+
