0ca4b42d9a0c8d2dbcee752ee6f55b0a276678d9
Mario Mello <mario@dindigital.com>
2014-01-22 16:58:15 -0200

    Adicionando Filter HTML para Scape

---
+++ library/Din/Filters/String/Html.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Din\Filters\String;
+
+class Html
+{
+
+  public static function scape ( $string )
+  {
+    $scaped_string = htmlspecialchars($string);
+
+    return $scaped_string;
+  }
+
+}
+
