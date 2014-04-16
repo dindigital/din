a23ae7ed4df9e9358574f8cd06048c4605e10608
Jair Cueva Junior <junior@dindigital.com>
2014-04-16 16:02:52 -0300

    Adiciona ao Filter Intval a funcionalidade de default value com  padrao em null

--- library/Din/TableFilter/Filters/Intval.php
+++ library/Din/TableFilter/Filters/Intval.php
@@ -7,9 +7,23 @@ use Din\TableFilter\AbstractFilter;
 class Intval extends AbstractFilter
 {
 
+  protected $_default_value;
+
+  public function __construct ( $default_value = null )
+  {
+    $this->_default_value = $default_value;
+
+  }
+
   public function filter ( $field )
   {
-    $value = intval($this->getValue($field));
+    $v = $this->getValue($field);
+
+    if ( is_null($v) ) {
+      $value = $this->_default_value;
+    } else {
+      $value = intval($v);
+    }
 
     $this->_table->{$field} = $value;
 
