87f7a0d02f2027d607441f13cde6c1d89856b24b
Jair Cueva Junior <junior@dindigital.com>
2014-05-10 15:19:58 -0300

    Adiciona na Header getUrl

--- library/Din/Http/Header.php
+++ library/Din/Http/Header.php
@@ -28,6 +28,21 @@ class Header
 
   }
 
+  public static function getUrl ()
+  {
+    $s = $_SERVER;
+    $ssl = (!empty($s['HTTPS']) && $s['HTTPS'] == 'on') ? true : false;
+    $sp = strtolower($s['SERVER_PROTOCOL']);
+    $protocol = substr($sp, 0, strpos($sp, '/')) . (($ssl) ? 's' : '');
+    $port = $s['SERVER_PORT'];
+    $port = ((!$ssl && $port == '80') || ($ssl && $port == '443')) ? '' : ':' . $port;
+    //$host = ($use_forwarded_host && isset($s['HTTP_X_FORWARDED_HOST'])) ? $s['HTTP_X_FORWARDED_HOST'] : (isset($s['HTTP_HOST']) ? $s['HTTP_HOST'] : null);
+    $host = isset($host) ? $host : $s['SERVER_NAME'] . $port;
+
+    return $protocol . '://' . $host . self::getUri();
+
+  }
+
   public static function set404 ()
   {
     header("HTTP/1.0 404 Not Found");
