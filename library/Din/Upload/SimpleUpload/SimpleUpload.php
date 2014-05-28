3f40749b7bb9053afca18197473372bc3a6b7c3e
mariosmello <contato@mariosmello.com>
2014-05-27 23:16:50 -0300

    Inicio de testes

--- library/Din/Upload/SimpleUpload/SimpleUpload.php
+++ library/Din/Upload/SimpleUpload/SimpleUpload.php
@@ -2,12 +2,153 @@
 
 namespace Din\Upload\SimpleUpload;
 
+use Din\File\Folder;
+
 class SimpleUpload
 {
 
-  public function oi ()
+  protected $file = null;
+  protected $max_filesize = 2097152;
+  protected $file_type = 'image';
+  protected $file_validation = array(
+      'image' => array(
+          'mime' => array('image/png', 'image/jpeg'),
+          'extension' => array('jpg', 'jpeg', 'png'),
+      )
+  );
+  protected $extension;
+  protected $path;
+  protected $name;
+
+  public function setFileType ( $type )
   {
-    return 'oi';
+    $validation = $this->getFileValidation();
+    if ( !array_key_exists($type, $validation) )
+      throw new \InvalidArgumentException('Formato de validação inválido');
+
+    $this->file_type = $type;
+
+  }
+
+  public function getFileType ()
+  {
+    return $this->file_type;
+
+  }
+
+  public function getFileValidation ()
+  {
+    return $this->file_validation;
+
+  }
+
+  public function setFile ( $file = null )
+  {
+    if ( is_null($file) || $file == '' )
+      return null;
+
+    if ( !is_array($file) )
+      throw new \InvalidArgumentException('Arquivo inválido');
+
+    if ( !count($file) )
+      throw new \InvalidArgumentException('Arquivo inválido');
+
+    if ( !isset($file['name']) || !isset($file['type']) || !isset($file['size']) || !isset($file['tmp_name']) )
+      throw new \InvalidArgumentException('Arquivo inválido');
+
+    $this->file = $file;
+
+    $this->validationMimeFile();
+    $this->validationExtensionFile();
+    $this->validationFileSize();
+
+  }
+
+  public function getFile ()
+  {
+    return $this->file;
+
+  }
+
+  /**
+   * Configura o máximo de um arquivo
+   * usar número em megas
+   * Ex: 5 para 5 mb de limite, 10 para 10 mb de limite
+   */
+  public function setMaxFileZize ( $size )
+  {
+    if ( !is_numeric($size) )
+      throw new \InvalidArgumentException('O limite do arquivo deve ser um número');
+
+    $this->max_filesize = 1440000 * $size;
+
+  }
+
+  public function getMaxFileZize ()
+  {
+    return $this->max_filesize;
+
+  }
+
+  public function setPath ( $path )
+  {
+    try {
+      Folder::make_writable($path);
+      $this->path = $path;
+    } catch (\Exception $e) {
+      throw new \InvalidArgumentException('Caminho de destino inválido');
+    }
+
+  }
+
+  public function getPath ()
+  {
+    return $this->path;
+
+  }
+
+  public function setName ( $name = null )
+  {
+    if ( is_null($name) ) {
+      $name = uniqid();
+    }
+
+    $name .= ".{$this->extension}";
+
+    $this->name = $name;
+
+  }
+
+  public function getName ()
+  {
+    return $this->name;
+
+  }
+
+  protected function validationMimeFile ()
+  {
+    $validation = $this->getFileValidation();
+    if ( !in_array($this->file['type'], $validation[$this->getFileType()]['mime']) )
+      throw new \InvalidArgumentException('MIME inválido');
+
+  }
+
+  protected function validationExtensionFile ()
+  {
+    $validation = $this->getFileValidation();
+    $extension = pathinfo($this->file['name'], PATHINFO_EXTENSION);
+    if ( !in_array(strtolower($extension), $validation[$this->getFileType()]['extension']) )
+      throw new \InvalidArgumentException('Extensão de arquivo inválida');
+
+    $this->extension = $extension;
+
+  }
+
+  protected function validationFileSize ()
+  {
+
+    if ( $this->getMaxFileZize() < $this->file['size'] )
+      throw new \InvalidArgumentException("Tamanho de arquivo inválido.");
 
   }
 
--- tests/library/Din/Upload/SimpleUpload/SimpleUploadTest.php
+++ tests/library/Din/Upload/SimpleUpload/SimpleUploadTest.php
@@ -2,12 +2,238 @@
 
 namespace Din\Upload\SimpleUpload\tests;
 
-class SimpleUploadTest
+use Din\File\Folder;
+
+class SimpleUploadTest extends \PHPUnit_Framework_TestCase
 {
 
-  function __construct ()
+  protected $simpleUpload;
+
+  public function __construct ()
   {
+
+    $this->simpleUpload = new \Din\Upload\SimpleUpload\SimpleUpload;
+    $test = $this->simpleUpload->getFile();
+    $this->assertNull($test);
+
+    $test = $this->simpleUpload->getMaxFileZize();
+    $this->assertEquals(2097152, $test);
+
+    $test = $this->simpleUpload->getFileType();
+    $this->assertEquals('image', $test);
 
   }
 
+  public function testFileNull ()
+  {
+    $test = $this->simpleUpload->setFile();
+    $this->assertNull($test);
+
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testSetFileNotArray ()
+  {
+    $this->simpleUpload->setFile('a');
+
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testSetFileEmptyArray ()
+  {
+    $this->simpleUpload->setFile(array());
+
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testSetFileInvalidArray ()
+  {
+    $this->simpleUpload->setFile(array('foo' => 'bar'));
+
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testSetInvalidFileType ()
+  {
+    $this->simpleUpload->setFileType('exe');
+
+  }
+
+  public function testSetFileType ()
+  {
+    $this->simpleUpload->setFileType('image');
+    $test = $this->simpleUpload->getFileType();
+    $this->assertEquals('image', $test);
+
+  }
+
+  public function testSetFile ()
+  {
+
+    $_FILES = array(
+        'test' => array(
+            'name' => 'test.jpg',
+            'type' => 'image/jpeg',
+            'size' => 542,
+            'tmp_name' => '',
+            'error' => 0
+        )
+    );
+
+    $this->simpleUpload->setFile($_FILES['test']);
+    $test = $this->simpleUpload->getFile();
+    $this->assertNotNull($test);
+
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testSetBigFile ()
+  {
+
+    $_FILES = array(
+        'test' => array(
+            'name' => 'test.jpg',
+            'type' => 'image/jpeg',
+            'size' => 12312312312312,
+            'tmp_name' => '//',
+            'error' => 0
+        )
+    );
+
+    $this->simpleUpload->setFile($_FILES['test']);
+
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testMimeFile ()
+  {
+
+    $_FILES = array(
+        'test' => array(
+            'name' => 'test.jpg',
+            'type' => 'pdf',
+            'size' => 1000,
+            'tmp_name' => '',
+            'error' => 0
+        )
+    );
+
+    $this->simpleUpload->setFile($_FILES['test']);
+
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testExtensionFile ()
+  {
+
+    $_FILES = array(
+        'test' => array(
+            'name' => 'test.pdf',
+            'type' => 'image/jpeg',
+            'size' => 1000,
+            'tmp_name' => '',
+            'error' => 0
+        )
+    );
+
+    $this->simpleUpload->setFile($_FILES['test']);
+
+  }
+
+  public function testSetMaxFileSize ()
+  {
+    $this->simpleUpload->setMaxFileZize(10);
+    $test = $this->simpleUpload->getMaxFileZize();
+    $this->assertEquals(14400000, $test);
+
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testSetMaxFileSizeExceptior ()
+  {
+    $this->simpleUpload->setMaxFileZize('a');
+
+  }
+
+  public function testeSetPath ()
+  {
+    $this->simpleUpload->setPath(__DIR__ . '/files');
+    $test = $this->simpleUpload->getPath();
+    $this->assertEquals(__DIR__ . '/files', $test);
+    Folder::delete(__DIR__ . '/files');
+
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testeSetInvalidPath ()
+  {
+    $this->simpleUpload->setPath('/invalidpath');
+
+  }
+
+  /**
+   * @depends testSetFile
+   */
+  public function testSetName ()
+  {
+    $this->simpleUpload->setName('arquivo');
+    $test = $this->simpleUpload->getName();
+    $this->assertNotEmpty($test);
+
+  }
+
+  /**
+   * @depends testSetFile
+   */
+  public function testSetNullName ()
+  {
+    $this->simpleUpload->setName();
+    $test = $this->simpleUpload->getName();
+    $this->assertNotEmpty($test);
+
+  }
+
+  /* protected function createFile ()
+    {
+    Folder::make_writable(__DIR__ . '/uploads');
+    $im = @imagecreatetruecolor(50, 100);
+    imagejpeg($im, __DIR__ . '/uploads/test.jpg');
+
+    }
+
+    public function testMove ()
+    {
+    $_FILES = array(
+    'test' => array(
+    'name' => 'test.jpg',
+    'type' => 'image/jpeg',
+    'size' => 542,
+    'tmp_name' => __DIR__ . '/uploads/test.jpg',
+    'error' => 0
+    )
+    );
+
+    $this->createFile();
+    $this->simpleUpload->setFile($_FILES['test']);
+    $this->simpleUpload->setPath(__DIR__ . '/files');
+
+    } */
 }
