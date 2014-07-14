c234a96d484a4554fdcf37c7002d284e36448133
Jair Cueva Junior <junior@dindigital.com>
2014-07-14 15:46:05 -0300

    Adiciona possibilidade de validar/filtrar data somente se tiver preenchido ou se for obrigatorio atraves de parametro

--- library/Din/InputValidator/Validators/Date.php
+++ library/Din/InputValidator/Validators/Date.php
@@ -9,16 +9,21 @@ class Date extends AbstractValidator
 {
 
   protected $_format;
+  protected $_required;
 
-  public function __construct ( $format = 'd/m/Y' )
+  public function __construct ( $format = 'd/m/Y', $required = true )
   {
     $this->_format = $format;
+    $this->_required = $required;
 
   }
 
   public function validate ( $prop, $label )
   {
     $value = $this->getValue($prop);
+
+    if ( $value == '' && !$this->_required )
+      return;
 
     if ( !v::date($this->_format)->validate($value) )
       $this->addException("Campo {$label} com uma data inválida");
--- library/Din/TableFilter/Filters/Date.php
+++ library/Din/TableFilter/Filters/Date.php
@@ -8,11 +8,32 @@ use Din\Filters\Date\DateToSql;
 class Date extends AbstractFilter
 {
 
+  protected $_with_time;
+
+  public function __construct ( $with_time = false )
+  {
+    $this->_with_time = $with_time;
+
+  }
+
   public function filter ( $field )
   {
     $value = $this->getValue($field);
 
-    $this->_table->{$field} = DateToSql::filter_date($value);
+    if ( (string) $value == '' ) {
+      $this->_table->{$field} = null;
+      return;
+    }
+
+    if ( $this->_with_time ) {
+      $explode = explode(' ', $value);
+      $date = $explode[0];
+      $time = $explode[1];
+
+      $this->_table->{$field} = DateToSql::filter_date($date) . ' ' . $time;
+    } else {
+      $this->_table->{$field} = DateToSql::filter_date($value);
+    }
 
   }
 
